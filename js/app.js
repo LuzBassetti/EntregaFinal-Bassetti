3dc5cf41fc894825bf9022c4f2c8cb19bd275330
Luz Bassetti <luzbassetti95@gmail.com>
2023-09-06 00:24:12 -0300

    carrito con storage

--- js/app.js
+++ js/app.js
@@ -1,113 +1,200 @@
-function mostrarProductos() {
-    let productosText = "Detallamos a continuacion nuestros productos y sus precios:\n";
-    productos.forEach(producto => {
-        productosText += `${producto.nombre}:`;
-        productosText += ` -${producto.size}....$${producto.precio}\n`;
-    });
-    alert(productosText);
-}
-
-function precioTotal() {
-    let total = 0
-    for (let i = 0; i < productos.length; i++) {
-        total += (productos[i].precio * productos[i].cantidad);
-    }
-    return total;
-}
-
 class producto {
-    constructor(nombreId, nombre, sizeId, size, precio) {
-        this.nombreId = nombreId;
+    constructor(id, nombre, precio, imagen, size) {
+        this.id = id;
         this.nombre = nombre;
-        this.sizeId = sizeId;
-        this.size = size;
         this.precio = precio;
-        this.cantidad = 0;
+        this.imagen = imagen;
+        this.size = size;
     }
 }
+
 
 const productos = [];
-productos.push(new producto("1", "Chocotorta", "1", "Pequeña", "4000"));
-productos.push(new producto("1", "Chocotorta", "2", "Mediana", "5500"));
-productos.push(new producto("1", "Chocotorta", "3", "Grande", "7000"));
-productos.push(new producto("2", "Cheesecake", "1", "Pequeño", "4500"));
-productos.push(new producto("2", "Cheesecake", "2", "Mediano", "6000"));
-productos.push(new producto("2", "Cheesecake", "3", "Grande", "8000"));
-productos.push(new producto("3", "Lemon Pie", "1", "Pequeño", "3500"));
-productos.push(new producto("3", "Lemon Pie", "2", "Mediano", "4500"));
-productos.push(new producto("3", "Lemon Pie", "3", "Grande", "6500"));
+productos.push(new producto(1, "Chocotorta", 4000, "img/chocotorta.jpeg", "Pequeña",));
+productos.push(new producto(2, "Chocotorta", 5500, "img/chocotorta.jpeg", "Mediana"));
+productos.push(new producto(3, "Chocotorta", 7000, "img/chocotorta.jpeg", "Grande"));
+productos.push(new producto(4, "Cheesecake", 4500, "img/cheesecake.jpeg", "Pequeño"));
+productos.push(new producto(5, "Cheesecake", 6000, "img/cheesecake.jpeg", "Mediano"));
+productos.push(new producto(6, "Cheesecake", 8000, "img/cheesecake.jpeg", "Grande"));
+productos.push(new producto(7, "Lemon Pie", 3500, "img/lemon.jpeg", "Pequeño"));
+productos.push(new producto(8, "Lemon Pie", 4500, "img/lemon.jpeg", "Mediano"));
+productos.push(new producto(9, "Lemon Pie", 6500, "img/lemon.jpeg", "Grande"));
 
-let nombreUser;
-let sizeName;
-let productName;
-let sizeUser;
-let decision;
-let opcion;
-let precio;
+productos.forEach(producto => {
+    console.log(producto)
+});
+
+let carrito = [];
+const divisa = '$';
+const DOMitems = document.getElementById('items');
+const DOMcarrito = document.getElementById('carrito');
+const DOMtotal = document.getElementById('total');
+const DOMbotonVaciar = document.getElementById('boton-vaciar');
+const miLocalStorage = window.localStorage;
 
 
-alert("Bienvenido a `Pasteleria y decoracion`");
-opcion = prompt("Menu de opciones: \n1-Comprar \n2-Contactanos \n3-Salir");
+// Funciones
 
-while ((opcion != "1") && (opcion != "2") && (opcion != "3")) {
-    alert("Opcion ingresada no válida. Por favor ingrese un número del 1 al 3.");
-    opcion = prompt("Menu de opciones: 1-Comprar 2-Contactanos 3-Salir");
+/**
+ * Dibuja todos los productos a partir de la base de datos. No confundir con el carrito
+ */
+function renderizarProductos() {
+    productos.forEach((info) => {
+        // Estructura
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        // Body
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // Titulo
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+        //Tamanio
+        const miNodoSize = document.createElement('h6');
+        miNodoSize.classList.add('card-text');
+        miNodoSize.textContent = info.size;
+        // Imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${divisa}${info.precio}`;
+        // Boton 
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', agregarProductoAlCarrito);
+        // Inserto
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoSize);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+    });
 }
-switch (opcion) {
-    case "1":
-        do {
-            mostrarProductos()
-            nombreUser = prompt("Ingrese el numero correspondiente al producto elegido (1 a 3)\nproducto : \n1-Chocotorta \n2-Cheesecake \n3-Lemon Pie");
-            while ((nombreUser != "1") && (nombreUser != "2") && (nombreUser != "3")) {
-                alert("Opcion ingresada no válida. Por favor ingrese un número del 1 al 3.");
-                nombreUser = prompt("Ingrese el número correspondiente al producto elegido (1 a 3)\nproducto : \n1-Chocotorta \n2-Cheesecake \n3-Lemon Pie");
-            }
 
-            productos.forEach(producto => {
-                if (producto.nombreId === nombreUser) {
-                    productName = producto.nombre;
-                }
-            });
+/**
+ * Evento para añadir un producto al carrito de la compra
+ */
+function agregarProductoAlCarrito(evento) {
+    // Agrego el Nodo a nuestro carrito
+    carrito.push(evento.target.getAttribute('marcador'))
+    // Actualiza el carrito 
+    renderizarCarrito();
+    // Actualiz el LocalStorage
+    guardarCarritoEnLocalStorage();
 
-            alert("Usted eligió el producto " + productName + ". Por favor ahora elija el tamaño:");
-            sizeUser = prompt("Ingrese el número correspondiente al tamaño elegido (1 a 3) \nTamaños: \n1-Pequeño \n2-Mediano \n3-Grande");
-            while ((sizeUser != "1") && (sizeUser != "2") && (sizeUser != "3")) {
-                alert("Opcion ingresada no válida. Por favor ingrese un número del 1 al 3.");
-                size = prompt("Ingrese el número correspondiente al tamaño elegido (1 a 3) \nTamaños: \n1-Pequeño \n2-Mediano \n3-Grande");
-            }
-
-            productos.forEach(producto => {
-                if ((producto.sizeId === sizeUser) && (producto.nombreId === nombreUser)) {
-                    sizeName = producto.size;
-                    console.log(sizeName);
-                    producto.cantidad++;
-                    precio = producto.precio;
-                }
-            });
-
-            alert("Usted eligió el producto " + productName + " y el tamaño " + sizeName + " \n por el precio de " + precio + "\n \n El total de la compra es de " + precioTotal());
-
-            decision = prompt("Desdea seguir comprando? (si/no)").toLowerCase();
-            while ((decision != "si") && (decision != "no")) {
-                console.log("desicion");
-                alert("Opcion no válida, por favor intente nuevamente");
-                decision = prompt("Desdea seguir comprando? (si/no)").toLowerCase();
-            }
-        } while (decision != "no");
-
-
-        alert("Gracias por su compra!");
-        break;
-
-    case "2":
-        alert("Para comunicarse con nosotros puede hacerlo a traves de Whatsapp al 34139999999");
-        break;
-
-    case "3":
-        alert("Gracias por visitar nuestro sitio. Lo esperamos nuevamente!");
-        break;
-
-    default:
-        alert("Opcion ingresada no válida. Hasta luego.");
-        break;
 }
+
+/**
+ * Dibuja todos los productos guardados en el carrito
+ */
+function renderizarCarrito() {
+    // Vacio todo el html
+    DOMcarrito.textContent = '';
+    // Saco los duplicados
+    const carritoSinDuplicados = [...new Set(carrito)];
+    // Genero los Nodos a partir de carrito
+    console.log(carritoSinDuplicados)
+    carritoSinDuplicados.forEach((item) => {
+        // Obtengo el item que necesitamos de la variable base de datos
+        const miItem = productos.filter((itemBaseDatos) => {
+            // Coincide las id? Solo puede existir un caso
+            return itemBaseDatos.id === parseInt(item);
+        });
+        console.log(miItem)
+        // Cuenta el número de veces que se repite el producto
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            // Coincide las id? Incremento el contador, en caso contrario no mantengo
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        console.log(miItem)
+        console.log(numeroUnidadesItem)
+        // Creo el nodo del item del carrito
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].size[0]} - ${divisa}${miItem[0].precio}`;
+        // Boton de borrar
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        // Mezclo nodos
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+    // Renderizo el precio total en el HTML
+    DOMtotal.textContent = calcularTotal();
+}
+
+/**
+ * Evento para borrar un elemento del carrito
+ */
+function borrarItemCarrito(evento) {
+    // Obtengo el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Borro todos los productos
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    // vuelvo a renderizar
+    renderizarCarrito();
+    // Actualizo el LocalStorage
+    guardarCarritoEnLocalStorage();
+}
+
+/**
+ * Calcula el precio total teniendo en cuenta los productos repetidos
+ */
+function calcularTotal() {
+    // Recorro el array del carrito 
+    return carrito.reduce((total, item) => {
+        // De cada elemento obtenemos su precio
+        const miItem = productos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Los sumo al total
+        return total + miItem[0].precio;
+    }, 0);
+}
+
+/**
+ * Vacia el carrito y vuelve a dibujarlo
+ */
+function vaciarCarrito() {
+    // Limpio los productos guardados
+    carrito = [];
+    // Renderizo los cambios
+    renderizarCarrito();
+    // Borra LocalStorage
+    localStorage.clear();
+}
+
+function guardarCarritoEnLocalStorage () {
+    miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+function cargarCarritoDeLocalStorage () {
+    // Existe un carrito previo guardado en LocalStorage?
+    if (miLocalStorage.getItem('carrito') !== null) {
+        // Carga la información
+        carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+    }
+}
+
+// Eventos
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+// Inicio
+cargarCarritoDeLocalStorage();
+renderizarProductos();
+renderizarCarrito();
+
